d36bf5eafd3d7ebef52dc329af7e4f6861f7c12a
basildevops <eldhodevops1@gmail.com>
2025-04-07 08:01:36 +0530

    Create test_model_api_local.py

---
+++ app/test_model_api_local.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# Define API endpoint
+url = "http://127.0.0.1:5000/invocations"
+
+# Prepare input data
+data = {
+    "instances": [[1200], [2000]]      # Input data
+}
+
+# Send POST request
+response = requests.post(url, json=data)
+
+# Print response
+print("Predictions:", response.json())
