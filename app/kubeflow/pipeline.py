1bc62c487f651844580f09a0e9b99309a2036223
basildevops <eldhodevops1@gmail.com>
2025-04-22 07:05:08 +0530

    Update pipeline.py

--- app/kubeflow/pipeline.py
+++ app/kubeflow/pipeline.py
@@ -4,9 +4,15 @@ from kfp.dsl import pipeline
 from kfp import compiler
 from kfp.components import load_component_from_file
 
-@pipeline(name="Simple Linear Regression Pipeline", description="A basic regression model training pipeline.")
+@pipeline(
+    name="Simple Linear Regression Pipeline",
+    description="Train a linear regression model using Kubeflow Pipelines."
+)
 def regression_pipeline():
     train_op = load_component_from_file("train_component.yaml")()
-
+    
 if __name__ == "__main__":
-    compiler.Compiler().compile(regression_pipeline, "regression_pipeline.yaml")
+    compiler.Compiler().compile(
+        pipeline_func=regression_pipeline,
+        package_path="regression_pipeline.yaml"
+    )
