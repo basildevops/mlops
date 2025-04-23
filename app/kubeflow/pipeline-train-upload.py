658b5b91402c4d77a09a2ff9c8d5fb9c1c73fa7a
basildevops <eldhodevops1@gmail.com>
2025-04-23 08:03:18 +0530

    Create pipeline-train-upload.py

---
+++ app/kubeflow/pipeline-train-upload.py
@@ -0,0 +1,18 @@
+# pipeline.py
+
+from kfp import compiler
+from kfp.dsl import pipeline
+from component-s3 import train_component  # directly import decorated function
+
+@pipeline(
+    name="Simple Linear Regression Pipeline",
+    description="Train a linear regression model using Kubeflow Pipelines."
+)
+def regression_pipeline():
+    train_component()  # call the component directly
+
+if __name__ == "__main__":
+    compiler.Compiler().compile(
+        pipeline_func=regression_pipeline,
+        package_path="regression_pipeline_train_upload.yaml"
+    )
